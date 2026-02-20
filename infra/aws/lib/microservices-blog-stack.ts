c1a8b5425f19e996e096e653ad4d64dced430a90
yishai <yishai5555@gmail.com>
2026-02-20 15:58:25 +0200

    05-ecr

--- infra/aws/lib/microservices-blog-stack.ts
+++ infra/aws/lib/microservices-blog-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
 import { Construct } from 'constructs';
 import { VpcConstruct } from './vpc-stack';
 
@@ -6,10 +7,44 @@ export class MicroservicesBlogStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // Create VPC and Networking infrastructure
+    // Phase 2: VPC and networking
     const vpc = new VpcConstruct(this, 'Vpc');
 
-    // TODO: Phase 3 - ECR Repositories
+    // Phase 3: ECR repositories for all microservices
+    const services = [
+      'client',
+      'posts',
+      'comments',
+      'query',
+      'moderation',
+      'event-bus',
+    ] as const;
+
+    const ecrRepos: Record<(typeof services)[number], ecr.Repository> = {} as any;
+
+    services.forEach((service) => {
+      ecrRepos[service] = new ecr.Repository(this, `EcrRepo${toPascalCase(service)}`, {
+        repositoryName: `microservices-blog/${service}`,
+        imageScanOnPush: true,
+      });
+    });
+
+    // Optional: outputs for convenience
+    Object.entries(ecrRepos).forEach(([service, repo]) => {
+      new cdk.CfnOutput(this, `EcrRepoUri${toPascalCase(service)}`, {
+        value: repo.repositoryUri,
+        description: `ECR repository URI for ${service}`,
+      });
+    });
+
     // TODO: Phase 4 - EKS Cluster (will reference vpc.vpc, vpc.eksNodeSecurityGroup)
   }
+}
+
+function toPascalCase(name: string): string {
+  return name
+    .split(/[^a-zA-Z0-9]+/)
+    .filter(Boolean)
+    .map((part) => part.charAt(0).toUpperCase() + part.slice(1))
+    .join('');
 }
