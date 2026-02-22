38238dc06d9e53fd7f039771845869c684dae220
yishai <yishai5555@gmail.com>
2026-02-22 10:09:24 +0200

    06-fix-kubectlLayer

--- infra/aws/lib/eks-construct.ts
+++ infra/aws/lib/eks-construct.ts
@@ -1,3 +1,4 @@
+import { KubectlV28Layer } from '@aws-cdk/lambda-layer-kubectl-v28';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import * as eks from 'aws-cdk-lib/aws-eks';
 import { Construct } from 'constructs';
@@ -13,11 +14,14 @@ export class EksConstruct extends Construct {
   constructor(scope: Construct, id: string, props: EksConstructProps) {
     super(scope, id);
 
+    const kubectlLayer = new KubectlV28Layer(this, 'KubectlLayer');
+
     this.cluster = new eks.Cluster(this, 'EksCluster', {
       version: eks.KubernetesVersion.V1_28,
       vpc: props.vpc.vpc,
       vpcSubnets: [{ subnets: props.vpc.vpc.privateSubnets }],
       defaultCapacity: 0,
+      kubectlLayer,
     });
 
     // Managed node group: 2x t3.medium in private subnets
